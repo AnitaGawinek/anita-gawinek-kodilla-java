0ec6ed76ccb681275e0bbc7fd1aea1544520874e
Anita Gawinek <anitagawinek@gmail.com>
2019-04-17 19:54:28 +0200

    Excercise 6.6 resolved

--- kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
@@ -5,14 +5,27 @@ import java.util.List;
 
 public class BookLibrary {
     LibraryDatabase libraryDatabase;
+
     public BookLibrary(LibraryDatabase libraryDatabase) {
         this.libraryDatabase = libraryDatabase;
     }
-    public List<Book> listBooksWithCondition(String titleFragment){
-        List<Book> bookList = new ArrayList<Book>();
-        Book book = new Book("The book title", "The book author", 2000);
-        bookList.add(book);
-        // temporary returning list of one book
+
+    public List<Book> listBooksWithCondition(String titleFragment) {
+        List<Book> bookList = new ArrayList<>();
+        if (titleFragment.length() < 3) return bookList;
+        List<Book> resultList = libraryDatabase.listBooksWithCondition(titleFragment);
+        if (resultList.size() > 20) return bookList;
+        bookList = resultList;
         return bookList;
+    }
+
+    public List<Book> listBooksInHandsOf(LibraryUser libraryUser) {
+        List<Book> bookList = new ArrayList<>();
+        List<Book> resultList = libraryDatabase.listBooksInHandsOf(libraryUser);
+        if (resultList.size() == 0) {
+            return bookList;
+        } else {
+            return resultList;
+        }
     }
 }
