b6ad11b41c4c741c716b618975ff1989fc5b5caa
Anita Gawinek <anitagawinek@gmail.com>
2019-04-23 12:26:54 +0200

    Excercise 7.2 resolved

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.stream;
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -15,5 +16,8 @@ public class StreamMain {
                 poem.replaceAll(" ", "-"));
         poemBeautifier.beautify("This is my beautiful text!", poem ->
                 "*||* " + poem.toUpperCase().replace("BEAUTIFUL TEXT!", "BEST TEXT! :)") + " *||*" );
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
