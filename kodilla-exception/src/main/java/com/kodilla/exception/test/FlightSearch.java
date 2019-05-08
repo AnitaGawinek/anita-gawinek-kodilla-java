e3fa04db090d390fdffe88b5bacb5ad9f38d3c1c
Anita Gawinek <anitagawinek@gmail.com>
2019-05-08 14:37:19 +0200

    Excercise 8.4 corrected

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearch.java
@@ -1,36 +1,25 @@
 package com.kodilla.exception.test;
 import java.util.HashMap;
-import java.util.Random;
+import java.util.Map;
 
 public class FlightSearch {
     public boolean findFlight(Flight flight) throws RouteNotFoundException {
-        boolean isAvailable = true;
-        int results = new Random().nextInt(100);
-
         HashMap<String, Boolean> flights = new HashMap<>();
-        flights.put("London(LTN)", isAvailable);
-        flights.put("Katowice(KTW)", isAvailable);
-        flights.put("Monachium(MUC)", isAvailable);
-        flights.put("Berlin(TXL)", isAvailable);
-        flights.put("Dublin(DUB)", isAvailable);
-
-        if (!flights.keySet().contains(flight.getDepartureAirport()) || !flights.keySet().contains(flight.getArrivalAirport())) {
-            isAvailable = false;
-            results = 0;
-            System.out.println(results + " results found." + isAvailable);
-            throw new RouteNotFoundException();
+        flights.put("London(LTN)", true);
+        flights.put("Katowice(KTW)", false);
+        flights.put("Monachium(MUC)", false);
+        flights.put("Berlin(TXL)", true);
+        flights.put("Dublin(DUB)", true);
+        for (Map.Entry<String, Boolean> entry : flights.entrySet()) {
+            String k = entry.getKey();
+            Boolean v = entry.getValue();
+            if (k.equals(flight.getArrivalAirport())) {
+                System.out.println(v);
+                return v;
+            }
         }
-        if (flight.getDepartureAirport() == flight.getArrivalAirport()) {
-            isAvailable = false;
-            results = 0;
-            System.out.println("Departure and arrival airports are the same.\n" + results + " results found." +isAvailable);
-        } else {
-            isAvailable = true;
-            System.out.println(results + " results found." + isAvailable);
-        }
-        return isAvailable;
+        throw new RouteNotFoundException();
     }
-
     public static void main(String[] args){
         FlightSearch flightSearch = new FlightSearch();
         try {
