e590b1fb799e7a77b85a9311d6658c67270a57c0
Nitka <Nitka@iMac-mac.local>
2019-06-10 12:30:52 +0200

    Excercise 13.2

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/challenges/Application.java
@@ -0,0 +1,8 @@
+package com.kodilla.challenges;
+
+public class Application {
+
+    public static void main (String[] args){
+
+    }
+}
---
+++ kodilla-good-patterns/src/main/java/com/kodilla/challenges/ProductOrderService.java
@@ -0,0 +1,9 @@
+package com.kodilla.challenges;
+
+import java.time.LocalDateTime;
+
+public class ProductOrderService {
+    public boolean isOrdered(final User user, final LocalDateTime orderDate){
+        return true;
+    }
+}
